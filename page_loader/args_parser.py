f7deadaaadea7a59f7f294eef0db0ce3fd1206a1
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-11-24 11:44:18 +0200

    refactor args parsing

--- page_loader/args_parser.py
+++ page_loader/args_parser.py
@@ -1,12 +1,25 @@
+import os
 import argparse
 
 
-def get_parser():
+def parse_args():
+    parser = _get_parser()
+    args = parser.parse_args()
+    if not os.path.isabs(args.output):
+        output = os.path.join(os.getcwd(), args.output)
+    else:
+        output = args.output
+    return {'url': args.url, 'output': output}
+
+
+def _get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('URL', type=str)
+    parser.add_argument('url', type=str)
     parser.add_argument(
         '-o', '--output',
+        metavar='dir',
         type=str,
-        help='directory path where page will be saved'
+        help='output dir (default is the current directory)',
+        default=os.getcwd(),
     )
     return parser
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,24 +1,11 @@
-import os
 from page_loader.download import download
-from page_loader.args_parser import get_parser
+from page_loader.args_parser import parse_args
 
 
 def main():
-    parser = get_parser()
-    args = parser.parse_args()
-    dir_path = _get_dir_path(args.output)
-    file_path = download(args.URL, dir_path)
-    print(file_path)
-
-
-def _get_dir_path(output):
-    if output is None:
-        dir_path = os.getcwd()
-    elif not os.path.isabs(output):
-        dir_path = os.path.join(os.getcwd(), output)
-    else:
-        dir_path = output
-    return dir_path
+    args = parse_args()
+    page_path = download(args['url'], args['output'])
+    print(page_path)
 
 
 if __name__ == '__main__':
