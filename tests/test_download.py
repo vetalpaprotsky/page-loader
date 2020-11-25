d9f56f63ca657e2d2b7430d3117de4ae1b8ff92c
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-11-25 10:44:37 +0200

    refactor tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,3 +1,4 @@
+import os
 from tests.utils import load_fixture, whitespaces_removed
 from page_loader.download import download
 
@@ -15,17 +16,20 @@ def test_download_page_without_resources(requests_mock, tmpdir):
         expected = whitespaces_removed(page_content)
         assert page == expected
 
+    resources_dir_path = tmpdir / 'this-is-a-test-page_files'
+    assert not os.path.isdir(resources_dir_path)
+
 
 def test_download_page_with_resources(requests_mock, tmpdir):
     # css_file_content = 'h1 { color: red; }'
     # js_file_content = 'alert("JS");'
     image_binary = b'\xAB\xBC\xCD\xDE\xEF'
-    domain_url = 'https://test.com'
-    page_url = domain_url + '/page'
+    domain_url = 'https://ru.hexlet.io'
+    page_url = domain_url + '/courses'
 
     requests_mock.get(
         page_url,
-        text=load_fixture('test_page.html'),
+        text=load_fixture('hexlet_courses.html'),
     )
     # requests_mock.get(
     #     domain_url + '/assets/styles/main.css',
@@ -36,22 +40,25 @@ def test_download_page_with_resources(requests_mock, tmpdir):
     #     text=js_file_content
     # )
     requests_mock.get(
-        domain_url + '/assets/images/logo.png',
+        domain_url + '/assets/professions/python.png',
         content=image_binary,
     )
 
-    page_path = tmpdir / 'test-com-page.html'
+    page_path = tmpdir / 'ru-hexlet-io-courses.html'
     assert download(page_url, str(tmpdir)) == page_path
 
     with open(page_path) as file:
+        # TODO: Come up with a better way to check whether pages are equal.
         page = whitespaces_removed(file.read())
         expected = whitespaces_removed(
-            load_fixture('test_page_after_download.html')
+            load_fixture('hexlet_courses_after_download.html')
         )
-        assert page == expected
+        assert sorted(page) == sorted(expected)
 
-    resources_dir_path = tmpdir / 'test-com-page_files'
-    image_path = resources_dir_path / 'test-com-assets-images-logo.png'
+    resources_dir_path = tmpdir / 'ru-hexlet-io-courses_files'
+    image_path = (
+        resources_dir_path / 'ru-hexlet-io-assets-professions-python.png'
+    )
 
     with open(image_path, 'rb') as image:
         assert image.read() == image_binary
