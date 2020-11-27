eae3ea6d1abb3efb5cf3ff21c160ef0a111d2b42
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-11-27 09:05:55 +0200

    change tests for #download function

--- tests/test_download.py
+++ tests/test_download.py
@@ -20,29 +20,47 @@ def test_download_page_without_resources(requests_mock, tmpdir):
     assert not os.path.isdir(resources_dir_path)
 
 
+# TODO: refactor this.
 def test_download_page_with_resources(requests_mock, tmpdir):
-    # css_file_content = 'h1 { color: red; }'
-    # js_file_content = 'alert("JS");'
-    image_binary = b'\xAB\xBC\xCD\xDE\xEF'
-    domain_url = 'https://ru.hexlet.io'
-    page_url = domain_url + '/courses'
+    page_content = load_fixture('page.html')
+    application_css_content = 'h3 { color: red; }'
+    application_js_content = 'alert("Hexlet");'
+    runtime_js_content = 'alert("JS");'
+    python_png_binary = b'\xAB\xBC\xCD\xDE\xEF'
+    page_url = 'https://ru.hexlet.io/courses'
 
-    requests_mock.get(
-        page_url,
-        text=load_fixture('hexlet_courses.html'),
-    )
-    # requests_mock.get(
-    #     domain_url + '/assets/styles/main.css',
-    #     text=css_file_content
-    # )
-    # requests_mock.get(
-    #     domain_url + '/assets/scripts/main.js',
-    #     text=js_file_content
-    # )
-    requests_mock.get(
-        domain_url + '/assets/professions/python.png',
-        content=image_binary,
-    )
+    mocks = [
+        (
+            # the page itself and link href="/courses"
+            page_url,
+            page_content,
+        ),
+        (
+            # link href="/assets/application.css"
+            'https://ru.hexlet.io/assets/application.css',
+            application_css_content,
+        ),
+        (
+            # script src="/assets/application.js"
+            'https://ru.hexlet.io/assets/application.js',
+            application_js_content,
+        ),
+        (
+            # script src="https://ru.hexlet.io/packs/js/runtime.js"
+            'https://ru.hexlet.io/packs/js/runtime.js',
+            runtime_js_content,
+        ),
+        (
+            # img src="/assets/professions/python.png"
+            'https://ru.hexlet.io/assets/professions/python.png',
+            python_png_binary,
+        ),
+    ]
+    for url, content in mocks:
+        if type(content) is bytes:
+            requests_mock.get(url, content=content)
+        else:
+            requests_mock.get(url, text=content)
 
     page_path = tmpdir / 'ru-hexlet-io-courses.html'
     assert download(page_url, str(tmpdir)) == page_path
@@ -50,15 +68,19 @@ def test_download_page_with_resources(requests_mock, tmpdir):
     with open(page_path) as file:
         # TODO: Come up with a better way to check whether pages are equal.
         page = whitespaces_removed(file.read())
-        expected = whitespaces_removed(
-            load_fixture('hexlet_courses_after_download.html')
-        )
+        expected = whitespaces_removed(load_fixture('page_after_download.html'))
         assert sorted(page) == sorted(expected)
 
+    recourses = [
+        ('ru-hexlet-io-courses.html', page_content),
+        ('ru-hexlet-io-assets-application.css', application_css_content),
+        ('ru-hexlet-io-assets-application.js', application_js_content),
+        ('ru-hexlet-io-packs-js-runtime.js', runtime_js_content),
+        ('ru-hexlet-io-assets-professions-python.png', python_png_binary),
+    ]
     resources_dir_path = tmpdir / 'ru-hexlet-io-courses_files'
-    image_path = (
-        resources_dir_path / 'ru-hexlet-io-assets-professions-python.png'
-    )
-
-    with open(image_path, 'rb') as image:
-        assert image.read() == image_binary
+    for name, content in recourses:
+        recourse_path = resources_dir_path / name
+        read_mode = 'rb' if type(content) is bytes else 'r'
+        with open(recourse_path, read_mode) as file:
+            assert file.read() == content
