ea2658a772755f3d4f26458241d34b5466eba7b4
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-12-01 09:09:55 +0200

    fix tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,28 +1,27 @@
 import os
 from tests.utils import load_fixture, whitespaces_removed
-from page_loader.page import download
+from page_loader.download import download
 
 
 def test_download_page_without_resources(requests_mock, tmpdir):
-    page_content = '<html></html>'
+    page_html = '<html></html>'
     page_url = 'http://this.is.a.test/page'
-    requests_mock.get(page_url, text=page_content)
+    requests_mock.get(page_url, text=page_html)
 
     page_path = tmpdir / 'this-is-a-test-page.html'
     assert download(page_url, str(tmpdir)) == page_path
 
     with open(page_path) as file:
         page = whitespaces_removed(file.read())
-        expected = whitespaces_removed(page_content)
+        expected = whitespaces_removed(page_html)
         assert page == expected
 
     resources_dir_path = tmpdir / 'this-is-a-test-page_files'
     assert not os.path.isdir(resources_dir_path)
 
 
-# TODO: refactor this.
 def test_download_page_with_resources(requests_mock, tmpdir):
-    page_content = load_fixture('page.html')
+    page_html = load_fixture('page.html')
     application_css_content = 'h3 { color: red; }'
     application_js_content = 'alert("Hexlet");'
     runtime_js_content = 'alert("JS");'
@@ -33,7 +32,7 @@ def test_download_page_with_resources(requests_mock, tmpdir):
         (
             # the page itself and link href="/courses"
             page_url,
-            page_content,
+            page_html,
         ),
         (
             # link href="/assets/application.css"
@@ -57,7 +56,7 @@ def test_download_page_with_resources(requests_mock, tmpdir):
         ),
     ]
     for url, content in mocks:
-        if type(content) is bytes:
+        if isinstance(content, bytes):
             requests_mock.get(url, content=content)
         else:
             requests_mock.get(url, text=content)
@@ -71,16 +70,16 @@ def test_download_page_with_resources(requests_mock, tmpdir):
         expected = whitespaces_removed(load_fixture('page_after_download.html'))
         assert sorted(page) == sorted(expected)
 
-    recourses = [
-        ('ru-hexlet-io-courses.html', page_content),
+    resources = [
+        ('ru-hexlet-io-courses.html', page_html),
         ('ru-hexlet-io-assets-application.css', application_css_content),
         ('ru-hexlet-io-assets-application.js', application_js_content),
         ('ru-hexlet-io-packs-js-runtime.js', runtime_js_content),
         ('ru-hexlet-io-assets-professions-python.png', python_png_binary),
     ]
     resources_dir_path = tmpdir / 'ru-hexlet-io-courses_files'
-    for name, content in recourses:
-        recourse_path = resources_dir_path / name
-        read_mode = 'rb' if type(content) is bytes else 'r'
-        with open(recourse_path, read_mode) as file:
+    for name, content in resources:
+        resource_path = resources_dir_path / name
+        read_mode = 'rb' if isinstance(content, bytes) else 'r'
+        with open(resource_path, read_mode) as file:
             assert file.read() == content
