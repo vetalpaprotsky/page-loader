a13ff3cbe0caeb139e4e940f4da5cec597bb0156
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-12-03 10:35:05 +0200

    configure logger

--- page_loader/logging.py
+++ page_loader/logging.py
@@ -1,10 +1,12 @@
 import logging
 import sys
 
-logging.basicConfig(
-    format=('%(asctime)s - %(levelname)s - '
-            '%(filename)s:%(lineno)d - %(message)s'),
-    level=logging.INFO,
-    stream=sys.stderr,
-)
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+_handler = logging.StreamHandler(sys.stderr)
+_handler.setLevel(logging.INFO)
+_formatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s'
+)
+_handler.setFormatter(_formatter)
+logger.addHandler(_handler)
