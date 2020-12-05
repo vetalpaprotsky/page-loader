0d7312eabee6abc0c08997e39d61453939d7e971
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-12-05 15:50:08 +0200

    add tests for download function

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,21 +1,8 @@
+import pytest
 import os
 from tests.utils import load_fixture, is_content_identical
 from page_loader.download import download
-
-
-def test_download_page_without_resources(requests_mock, tmpdir):
-    page_html = '<html></html>'
-    page_url = 'http://this.is.a.test/page'
-    requests_mock.get(page_url, text=page_html)
-
-    page_path = tmpdir / 'this-is-a-test-page.html'
-    assert download(page_url, str(tmpdir)) == page_path
-
-    with open(page_path) as file:
-        assert is_content_identical(file.read(), page_html)
-
-    resources_dir_path = tmpdir / 'this-is-a-test-page_files'
-    assert not os.path.isdir(resources_dir_path)
+from page_loader.exceptions import HTTPError, DirectoryError
 
 
 def test_download_page_with_resources(requests_mock, tmpdir):
@@ -59,10 +46,10 @@ def test_download_page_with_resources(requests_mock, tmpdir):
         else:
             requests_mock.get(url, text=content)
 
-    page_path = tmpdir / 'ru-hexlet-io-courses.html'
-    assert download(page_url, str(tmpdir)) == page_path
+    page_file_path = tmpdir / 'ru-hexlet-io-courses.html'
+    assert download(page_url, str(tmpdir)) == page_file_path
 
-    with open(page_path) as file:
+    with open(page_file_path) as file:
         assert is_content_identical(
             file.read(),
             load_fixture('page_after_download.html')
@@ -81,3 +68,60 @@ def test_download_page_with_resources(requests_mock, tmpdir):
         read_mode = 'rb' if isinstance(content, bytes) else 'r'
         with open(resource_path, read_mode) as file:
             assert file.read() == content
+
+
+def test_download_page_with_some_unavailable_resources(requests_mock, tmpdir):
+    page_html = '''
+        <html>
+            <body>
+                <img src="available.png">
+                <img src="unavailable.png">
+            <body>
+        </html>
+    '''
+    page_url = 'http://test.com'
+    requests_mock.get(page_url, text=page_html)
+    requests_mock.get(page_url + '/available.png', content=b'\xFF')
+    requests_mock.get(page_url + '/unavailable.png', status_code=404)
+
+    download(page_url, str(tmpdir))
+
+    resources_dir_path = tmpdir / 'test-com_files'
+    assert os.path.isfile(tmpdir / 'test-com.html')
+    assert os.path.isfile(resources_dir_path / 'test-com-available.png')
+    assert not os.path.isfile(resources_dir_path / 'test-com-unavailable.png')
+
+
+def test_download_page_without_resources(requests_mock, tmpdir):
+    page_url = 'http://test.com'
+    requests_mock.get(page_url, text='<html></html>')
+
+    download(page_url, str(tmpdir))
+
+    resources_dir_path = tmpdir / 'test-com_files'
+    assert os.path.isfile(tmpdir / 'test-com.html')
+    assert not os.path.isdir(resources_dir_path)
+
+
+def test_download_unavailable_page(requests_mock, tmpdir):
+    page_url = 'http://test.com'
+    requests_mock.get(page_url, status_code=404)
+
+    with pytest.raises(HTTPError):
+        download(page_url, str(tmpdir))
+
+
+def test_download_when_resources_dir_cant_be_created(requests_mock):
+    page_html = '''
+        <html>
+            <body>
+                <script src="script.js"></script>
+            <body>
+        </html>
+    '''
+    page_url = 'http://test.com'
+    requests_mock.get(page_url, text=page_html)
+    requests_mock.get(page_url + '/script.js', text='console.log("JS");')
+
+    with pytest.raises(DirectoryError):
+        download(page_url, 'non/existing/dir/path')
