a164670b37664356d49cfaf0818484cbdde0a7d1
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-12-09 10:30:01 +0200

    rename create_file function and write_to_file

--- page_loader/utils.py
+++ page_loader/utils.py
@@ -3,14 +3,14 @@ from page_loader.logging import logger
 from page_loader.exceptions import FileError, DirectoryError
 
 
-def create_file(content, path):
+def write_to_file(content, path):
     try:
         write_mode = 'wb' if isinstance(content, bytes) else 'w'
         with open(path, write_mode) as file:
             file.write(content)
-        logger.info(f'Created file: {path}')
+        logger.info(f'Wrote to file {path}')
     except OSError as e:
-        logger.error(f'Failed to create file: {path} - {str(e)}')
+        logger.error(f'Failed to write to file {path} - {str(e)}')
         raise FileError()
 
 
@@ -18,7 +18,7 @@ def create_dir(path):
     try:
         if not os.path.exists(path):
             os.mkdir(path)
-            logger.info(f'Created directory: {path}')
+            logger.info(f'Created directory {path}')
     except OSError as e:
-        logger.error(f'Failed to create directory: {path} - {str(e)}')
+        logger.error(f'Failed to create directory {path} - {str(e)}')
         raise DirectoryError()
